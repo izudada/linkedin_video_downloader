5d64b307eff68ccb6ac2e531df48747039747085
Anthony <tonyudeagbala@gmail.com>
2023-03-03 09:53:12 +0100

    fix: flash message with condition

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, redirect, render_template, request, send_file, url_for
+    Blueprint, flash, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 from .linkedin_downloader import lk_downloader
@@ -7,13 +7,23 @@ from .linkedin_downloader import lk_downloader
 
 bp = Blueprint('downloader', __name__)
 
-
 @bp.route('/', methods = ['GET', 'POST'])
 def index():
+    """
+        An index function that accepts user input as a url
+        to a linkedin post and streams a video related to the post
+    """
+    #   post request condition
     if request.method == 'POST':
+        #   get user input 
         linkedin_post = request.form['url']
+        #   call lk_downloader and pass in user input
         message = lk_downloader(linkedin_post)
-        flash(message, 'danger')
-        return redirect(url_for('index'))
+        if message == "Downloded video sucessfully":
+            flash(message, 'success')
+        else:
+            flash(message, 'danger')
+        return redirect(url_for('index', loader="loader"))
+    #   get request condition
     else:
         return render_template('index.html')
