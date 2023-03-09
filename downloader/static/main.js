f1613a4a95fde7a61cf61285e6c763aa1c35ded6
Anthony <tonyudeagbala@gmail.com>
2023-03-10 00:43:04 +0100

    fix: remove spinner after download

--- downloader/static/main.js
+++ downloader/static/main.js
@@ -2,10 +2,22 @@ let loader = document.getElementById('video_loader');
 let submitButton = document.getElementById('submit');
 let heading1 = document.getElementsByTagName("h1")[0];
 
+
+// function to stop spinner after 10secs 
+function stopSpinner (){
+    // add hidden class from spinner
+    loader.classList.add('hidden');
+ }
+
 // add event listener on submit button
 submitButton.addEventListener('click', () => {
+
     // reduce the margin of the heading tag when spinner appears
     heading1.style.marginTop = "40vh";
     // remove hidden class from spinner
     loader.classList.remove('hidden');
+
 })
+
+// spinner timeout
+const myTimeout = setTimeout(stopSpinner, 8000);
