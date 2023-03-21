e693f6af19b518dc14a5dd61dde6e060197bbda9
Anthony <tonyudeagbala@gmail.com>
2023-03-21 15:03:42 +0100

    fix: validate input

--- downloader/static/main.js
+++ downloader/static/main.js
@@ -4,10 +4,26 @@ let action = document.getElementById("action-text");
 let anchorTag = document.getElementById("download-anchor");
 let urlInput = document.getElementById("url");
 
+const validateLinkedInPost = (url) => {
+    /*
+        A function that validates a linkedin post
+    */
+    var pattern = /\/posts\/([a-zA-Z0-9\-]+)/;
+    var match = url.match(pattern);
+    if (match && match[1]) {
+        var postId = match[1];
+        // You can now check if the postId is valid using the LinkedIn API or any other method
+        return true;
+    } else {
+        return false;
+    }
+      
+};
+
 
 $('#url').on('keyup input', function() {
     let url = document.getElementById("url").value;
-    if (url.length > 0){
+    if (validateLinkedInPost(url)) {
         loader.classList.remove('hidden');
         action.classList.remove("hidden");
         $.ajax({
@@ -24,5 +40,14 @@ $('#url').on('keyup input', function() {
                 console.log(error)
             }
         })
+    } else {
+        $('.flash').remove();
+        $("main").prepend(`
+            <div class="flash">
+                <div class="danger">Invalid url. Please paste a valid LinkedIn url</div>
+            </div>
+        `);
+        $('.flash').fadeOut(7000);
+        $('#url').val('');
     }
 });
